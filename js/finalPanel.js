0f2e388ff5507a5a4a5da39114ec7451ce827aab
Victor Bordo <vbordo724@gmail.com>
2016-02-20 10:38:58 -0600

    update final graph and animation

--- js/finalPanel.js
+++ js/finalPanel.js
@@ -1,4 +1,50 @@
 // Final Graph rendering
+// var layout = {
+//     title: 'A Title',
+//     xaxis:{
+//         //range: [1, 4],
+//         title: 'Number of Books',
+//         showgrid: true,
+//         //zeroline: false,
+//         tick0: 0,
+//         dtick: 1,
+//         ticklen: 4,
+//         tickwidth: 1,
+//     },
+//     yaxis:{
+//         title: 'Price of Books ($)',
+//         tick0: 0,
+//         dtick: 1,
+//         ticklen: 4,
+//         tickwidth: 1,
+//     }
+// };
+//
+// var supplyCurve = {
+//     x: [0,1,2,3,4],
+//     y: [4,3,2,1,0],
+//     type: 'scatter'
+// }
+//
+// var demandCurve = {
+//     x: [0,1,2,3,4],
+//     y: [0,1,2,3,4]
+// }
+//
+// var dash = {
+//     x:[0,1,2, 2,2],
+//     y:[2,2,2, 1,0],
+//     line: {
+//         color: 'green',
+//         dash: 'dot',
+//         width: 2
+//     }
+// }
+//
+// var econGraph = [supplyCurve, demandCurve, dash];
+//
+// Plotly.newPlot('finalGraph', econGraph, layout);
+
 var layout = {
     title: 'A Title',
     xaxis:{
@@ -32,31 +78,40 @@ var demandCurve = {
 }
 
 var dash = {
-    x:[0,1,2, 2,2],
-    y:[2,2,2, 1,0],
+    x:[0,1,2, 0],
+    y:[0,1,2, 2],
     line: {
-        color: 'green',
         dash: 'dot',
         width: 2
-    }
+    },
+    fill:'tozeroy',
 }
 
-var econGraph = [supplyCurve, demandCurve, dash];
+var dash2 = {
+    x:[0,1,2,1,0],
+    y:[2,2,2,3,4],
+    line: {
+        dash: 'dot',
+        width: 2
+    },
+    fill:'tozerox',
+}
+
+var econGraph = [supplyCurve, demandCurve, dash, dash2];
 
 Plotly.newPlot('finalGraph', econGraph, layout);
 
 
 
+
 // Animation for user interaction with panels corresponding to graph
-
-
 (function(){
 
     var highlightTraceColor = function(){
         var updateD3 = {
             line: {
-                color: 'purple',
-                width: 4.0
+                color: 'blue',
+                width: 8.0
             }
         };
         Plotly.restyle(finalGraph, updateD3, [0]);
@@ -88,8 +143,8 @@ Plotly.newPlot('finalGraph', econGraph, layout);
     var highlightTraceColorD4 = function(){
         var updateD4 = {
             line: {
-                color: 'purple',
-                width: 4.0
+                color: 'orange',
+                width: 8.0
             }
         };
         Plotly.restyle(finalGraph, updateD4, [1]);
