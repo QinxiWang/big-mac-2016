71dfff52ca37673e405be3369e7c38f57c6fb52d
pbertel <pbertel@macalester.edu>
2016-02-20 00:31:39 -0600

    Added better prompt box with bootbox, styled well

--- js/homepageJs.js
+++ js/homepageJs.js
@@ -1,4 +1,33 @@
 function alertName() {
-    var name = prompt("What is your name?");
+    console.log("hellobob");
+    bootbox.prompt("What is your name?", function(result) {
+      if (result === null) {
+        console.log("No answer");
+      } else {
+        var name = result;
+        console.log("Name is: " + name);
+        var fileName = "../html/test.html";
+        window.location = fileName;
+      }
+    });
     return name;
 };
+
+
+
+// bootbox.prompt("What is your name?", function(result) {
+//   if (result === null) {
+//     Example.show("Prompt dismissed");
+//   } else {
+//     name = result;
+//   }
+// });
+
+
+// bootbox.prompt("What is your name?", function(result) {
+//   if (result === null) {
+//     Example.show("Prompt dismissed");
+//   } else {
+//     Example.show("Hi <b>"+result+"</b>");
+//   }
+// });
