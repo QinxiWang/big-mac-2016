551e3a7f664766380d88e192376180d06237d016
Qinxi Wang <qwang@macalester.edu>
2016-02-20 04:20:13 -0600

    panels 4 through 6

---
+++ js/panel5.js
@@ -0,0 +1,45 @@
+var layout = {
+    title: 'A Title',
+    xaxis:{
+        //range: [1, 4],
+        title: 'Number of Books',
+        showgrid: true,
+        //zeroline: false,
+        tick0: 0,
+        dtick: 1,
+        ticklen: 4,
+        tickwidth: 1,
+    },
+    yaxis:{
+        title: 'Price of Books ($)',
+        tick0: 0,
+        dtick: 1,
+        ticklen: 4,
+        tickwidth: 1,
+    }
+};
+
+var supplyCurve = {
+    x: [0,1,2,3,4],
+    y: [4,3,2,1,0],
+    type: 'scatter'
+}
+
+var demandCurve = {
+    x: [0,1,2,3,4],
+    y: [0,1,2,3,4]
+}
+
+var dash = {
+    x:[3,2,1, 1,1],
+    y:[1,1,1, 2,3],
+    line: {
+        dash: 'dot',
+        width: 2
+    }
+}
+
+var econGraph = [supplyCurve, demandCurve, dash];
+
+
+Plotly.newPlot('panelGraph5', econGraph, layout);
