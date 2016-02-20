f5da5d35f6756e4bf9676818af84e53d835a292e
Victor Bordo <vbordo724@gmail.com>
2016-02-20 07:47:36 -0600

    getting initial toggle of graph line color

--- js/finalPanel.js
+++ js/finalPanel.js
@@ -1,9 +1,4 @@
-// $(function() {
-//     console.log("test")
-// });
-
-//alert("test")
-
+// Final Graph rendering
 var layout = {
     title: 'A Title',
     xaxis:{
@@ -40,6 +35,7 @@ var dash = {
     x:[0,1,2, 2,2],
     y:[2,2,2, 1,0],
     line: {
+        color: 'yellow',
         dash: 'dot',
         width: 2
     }
@@ -47,5 +43,32 @@ var dash = {
 
 var econGraph = [supplyCurve, demandCurve, dash];
 
+var graphRef = Plotly.newPlot('finalGraph', econGraph, layout);
 
-Plotly.newPlot('finalGraph', econGraph, layout);
+// Animation for user interaction with panels corresponding to graph
+var highlightTraceColor = function(){
+    var update = {
+        line: {color: 'green'}
+    };
+    Plotly.restyle(finalGraph, update, [0]);
+}
+
+var revertColor = function(){
+    var revertChange = {
+        line: {color: 'blue'}
+    };
+    Plotly.restyle(finalGraph, revertChange, [0]);
+}
+
+// working code 
+var n = 0;
+$( "div.enterleave" )
+  .mouseenter(function() {
+    n += 1;
+    highlightTraceColor();
+    $( this ).find( "span" ).text( "mouse enter x " + n );
+  })
+  .mouseleave(function() {
+      revertColor();
+    $( this ).find( "span" ).text( "mouse leave" );
+  });
