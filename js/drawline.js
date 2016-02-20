4efb673e100ffb241cc14abeea128f0cd7c0eb2f
Qinxi Wang <qwang@macalester.edu>
2016-02-20 03:06:48 -0600

    draw graph lines

---
+++ js/drawline.js
@@ -0,0 +1,74 @@
+
+var layout = {
+    title: 'A Title',
+    xaxis:{
+        //range: [1, 4],
+        title: 'Number of Books',
+        showgrid: true,
+        //zeroline: false,
+        tick0: 0,
+        dtick: 1,
+        ticklen: 4,
+        tickwidth: 1,
+    },
+    yaxis:{
+        title: 'Price of Books ($)',
+        tick0: 0,
+        dtick: 1,
+        ticklen: 4,
+        tickwidth: 1,
+    }
+};
+
+var supplyCurve = {
+    x: [0,1,2,3,4],
+    y: [4,3,2,1,0],
+    type: 'scatter'
+}
+
+var demandCurve = {
+    x: [0,1,2,3,4],
+    y: [0,1,2,3,4]
+}
+
+var dash = {
+    x:[1,1,1, 1,0],
+    y:[1,2,3, 3,3],
+    line: {
+        dash: 'dot',
+        width: 2
+    }
+}
+
+var econGraph = [supplyCurve, demandCurve, dash];
+
+
+Plotly.newPlot('tester', econGraph, layout);
+
+// var layout = {
+//     title: 'A Title',
+//     xaxis:{
+//         title: 'Number of Books',
+//         showgrid: true,
+//         zeroline: false
+//     },
+//     yaxis:{
+//         title: 'Price of Books ($)'
+//     }
+// };
+//
+// var supplyCurve = {
+//     x: [1, 2, 3, 4],
+//     y: [1, 2, 3, 4],
+//     type: 'scatter'
+// }
+//
+// var demandCurve = {
+//     x: [1, 2, 3, 4],
+//     y: [4, 3, 2, 1]
+// }
+//
+// var econGraph = [supplyCurve, demandCurve];
+//
+//
+// Plotly.newPlot('tester', econGraph, layout);
