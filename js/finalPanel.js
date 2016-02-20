3e3b5ea227e2727520fc6b7b92a4dfcc7c75a356
Victor Bordo <vbordo724@gmail.com>
2016-02-20 06:48:05 -0600

    swap out placeholder for final graph in final panel

--- js/finalPanel.js
+++ js/finalPanel.js
@@ -1,5 +1,51 @@
-$(function() {
-    console.log("test")
-});
+// $(function() {
+//     console.log("test")
+// });
 
 //alert("test")
+
+var layout = {
+    title: 'A Title',
+    xaxis:{
+        //range: [1, 4],
+        title: 'Number of Books',
+        showgrid: true,
+        //zeroline: false,
+        tick0: 0,
+        dtick: 1,
+        ticklen: 4,
+        tickwidth: 1,
+    },
+    yaxis:{
+        title: 'Price of Books ($)',
+        tick0: 0,
+        dtick: 1,
+        ticklen: 4,
+        tickwidth: 1,
+    }
+};
+
+var supplyCurve = {
+    x: [0,1,2,3,4],
+    y: [4,3,2,1,0],
+    type: 'scatter'
+}
+
+var demandCurve = {
+    x: [0,1,2,3,4],
+    y: [0,1,2,3,4]
+}
+
+var dash = {
+    x:[0,1,2, 2,2],
+    y:[2,2,2, 1,0],
+    line: {
+        dash: 'dot',
+        width: 2
+    }
+}
+
+var econGraph = [supplyCurve, demandCurve, dash];
+
+
+Plotly.newPlot('finalGraph', econGraph, layout);
